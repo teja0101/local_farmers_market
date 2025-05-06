9372caa94515008f6690cc2d5668d52119e086a0
LAKSHMIPURAM TEJA <140742318+teja0101@users.noreply.github.com>
2025-05-06 15:16:33 +0530

    Update conn.js

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
+import ENV from '../config.js';
 
-import { MongoMemoryServer } from "mongodb-memory-server";
-import ENV from '../config.js'
+async function connect() {
+  mongoose.set('strictQuery', true);
+  const db = await mongoose.connect(ENV.ATLAS_URI);
 
-async function connect(){
-
-    const mongod = await MongoMemoryServer.create();
-    mongoose.set('strictQuery', true)
-    // const db = await mongoose.connect(getUri);
-    const db = await mongoose.connect(ENV.ATLAS_URI);
-    console.log("Database Connected")
-    return db;
+  console.log("MongoDB Atlas connected");
+  return db;
 }
 
 export default connect;
